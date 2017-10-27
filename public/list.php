e8ed9268123771980b5138407759f07f555f3c2d
neby55 <ben@progweb.fr>
2017-10-27 17:12:41 +0200

    pagination started

--- public/list.php
+++ public/list.php
@@ -3,11 +3,28 @@
 require_once __DIR__.'/../inc/config.php';
 
 $studentList = array();
+// Page sasie dans l'URL
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+/*
+page => offset
+1 => 0
+2 => 3
+3 => 6
+4 => 9
+5 => 12
+*/
+// Je calcule automatiquement l'offset
+$offset = ($page-1) * 3;
+// Je corrige une possible erreur de calcul
+if ($offset < 0) {
+	$offset = 0;
+}
 
 $sql = '
 	SELECT *
 	FROM student
-';
+	LIMIT 3 OFFSET '.$offset;
 $pdoStatement = $pdo->query($sql);
 
 if ($pdoStatement === false) {
