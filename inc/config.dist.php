6ea504f227c5157eccf65dd3375253d36d9b17de
neby55 <ben@progweb.fr>
2017-10-30 09:07:18 +0100

    adding config.dist.php

---
+++ inc/config.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+// Données de configuration
+$config = array(
+	'DB_HOST' => '',
+	'DB_USER' => '',
+	'DB_PASSWORD' => '',
+	'DB_DATABASE' => ''
+);
+
+// Inclusions de fichiers
+require_once __DIR__.'/db.php';
+require_once __DIR__.'/functions.php';
+
+// Inclusion de composer (pour avoir toutes les libraires installés via composer)
+require_once __DIR__.'/../vendor/autoload.php';
+
+// Social Networks
+//Create a Page instance with the url information
+$socialLinksPage = new SocialLinks\Page([
+    'url' => 'http://projet-toto.dev',
+    'title' => 'Projet TOTO',
+    'text' => 'Extended page description',
+    'image' => 'http://mypage.com/image.png',
+    'icon' => 'http://mypage.com/favicon.png',
+    'twitterUser' => '@twitterUser'
+]);
+//print_r($socialLinksPage);
