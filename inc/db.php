75cc92c68b69d8db2fe43e1a1162cbd8c982db38
neby55 <ben@progweb.fr>
2017-10-30 16:06:31 +0100

    adding exception generation on every SQL errors

--- inc/db.php
+++ inc/db.php
@@ -7,6 +7,8 @@ $dsn = 'mysql:dbname='.$config['DB_DATABASE'].';host='.$config['DB_HOST'].';char
 try {
 	// Je crée une instance de PDO
 	$pdo = new PDO($dsn, $config['DB_USER'], $config['DB_PASSWORD']);
+	// Génère automatiquement une exception si erreur dans la requete
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (Exception $e) {
 	echo 'Connexion échouée : '.$e->getMessage();
